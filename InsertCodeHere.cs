5f7e0b2d486dac0ef4ce437a85fedbcd47f8131b
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:37:15 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DiceGame
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            DiceCup myDiceCup = new DiceCup();
+            Console.WriteLine($"In the DiceCup we rolled for a total of {myDiceCup.TotalValue}");
+            Console.WriteLine("Let us shake things up.");
+            myDiceCup.Shake();
+            Console.WriteLine($"Now we rolled for a total of {myDiceCup.TotalValue}");
+            Console.WriteLine("One more time");
+            myDiceCup.Shake();
+            Console.WriteLine($"Now we rolled for a total of {myDiceCup.TotalValue}");
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
